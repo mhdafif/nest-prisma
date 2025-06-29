c33ab1a5a65e04218370e9ce578a6b134d479593
Afif <aafif.mhd@gmail.com>
2025-06-29 23:23:08 +0700

    rename user guard to auth guard

--- src/auth/auth.guard.ts
+++ src/auth/auth.guard.ts
@@ -6,19 +6,34 @@ import {
 } from '@nestjs/common';
 import { Request } from 'express';
 import { JwtService } from '@nestjs/jwt';
-import { jwtConstants } from './constants';
+import { jwtConstants } from '../users/constants';
+import { Role } from 'src/roles/roles.enum';
+import { Reflector } from '@nestjs/core';
+import { IS_PUBLIC_KEY } from 'src/public/public.decorator';
 
 // the payload is depends on the login when creating the JWT token
 // in this case, we are using id and email as the payload
 interface JwtPayload {
   id: string;
   email: string;
+  role: Role;
 }
 
 @Injectable()
-export class UsersGuard implements CanActivate {
-  constructor(private jwtService: JwtService) {}
+export class AuthGuard implements CanActivate {
+  constructor(
+    private jwtService: JwtService,
+    private reflector: Reflector,
+  ) {}
   async canActivate(context: ExecutionContext): Promise<boolean> {
+    const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+    if (isPublic) {
+      return true;
+    }
+
     const request: Request = context.switchToHttp().getRequest<Request>();
     const token = this.getToken(request);
     if (!token) {
@@ -31,8 +46,9 @@ export class UsersGuard implements CanActivate {
         secret: jwtConstants.secret,
       },
     );
-    request['user'] = payload;
 
+    // data user (yang ada dipayload) dikirim ke users controller (bisa di akses dari req.user atau sejenisny. contoh ada di getProfile)
+    request['user'] = payload;
     return true;
   }
 
